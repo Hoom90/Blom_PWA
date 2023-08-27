871368c16c5a2399f81993d438d263128d23700d
Hoom90 <mohammad.mahdavi0015@gmail.com>
2023-08-27 14:08:37 +0330

    commit

--- src/components/addPlantView-GetPicture.vue
+++ src/components/addPlantView-GetPicture.vue
@@ -35,7 +35,17 @@ const setNewImage = (e) => {
     }
     const files = []
     for (let i = 0; i < e.target.files.length; i++) {
-        files.push(e.target.files[i])
+        let temp = e.target.files[i]
+
+        // Prompt the user to enter a new file name
+        // const newFileName = "user_" + localStorage.getItem('userId') + "_" + Date.now();
+        // filename.value.push(newFileName);
+        filename.value.push(temp.name);
+        // temp.name = newFileName
+
+        // Get file name and file itself to send to the database
+        file.value.push(temp);
+        files.push(temp)
     }
     files.forEach(item => {
         const reader = new FileReader();
@@ -43,27 +53,14 @@ const setNewImage = (e) => {
             userImages.value.push(event.target.result);
         };
         reader.readAsDataURL(item);
-
-        // Prompt the user to enter a new file name
-        const newFileName = "user_" + localStorage.getItem('userId') + "_" + Date.now();
-        // Change the file name of the selected file
-        // item.name = newFileName;
-        filename.value.push(item.name);
-
-        // Get file name and file itself to send to the database
-        const selectedFile = item;
-        file.value.push(selectedFile);
-
     });
-    console.log(file.value);
-    console.log(filename.value);
 };
 </script>
 <template>
     <div class="max-w-[400px] w-full h-full flex flex-col gap-2 p-[20px]">
 
         <div class="bg-gray-100 rounded-[20px] relative cursor-pointer" @click="openFileInput">
-            <img :src="serverURL + '/library/images/pic1.avif'" class="w-full h-full" loading="lazy" alt="عکس گرفتن">
+            <img :src="serverURL + '/library/images/pic1.avif'" class="w-full h-full" alt="عکس گرفتن">
             <div
                 class="absolute bottom-0 left-[50%] -translate-x-[50%] translate-y-[50%] flex justify-center items-center gap-3">
                 <div class="flex flex-col gap-3 jusctify-center items-center text-[12px]">
@@ -126,17 +123,16 @@ const setNewImage = (e) => {
             </div>
         </div>
 
-        <div>
-            <i>
+        <div class="text-[10px] flex gap-1 justify-center items-center mt-[80px]">
+            <i class="text-yellow-500">
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-                        stroke="#323232" stroke-width="2" />
-                    <path d="M12 8L12 13" stroke="#323232" stroke-width="2" stroke-linecap="round" />
-                    <path d="M12 16V15.9888" stroke="#323232" stroke-width="2" stroke-linecap="round" />
+                    <rect width="24" height="24" fill="white" />
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM11 13C11 13.5523 11.4477 14 12 14C12.5523 14 13 13.5523 13 13V8C13 7.44772 12.5523 7 12 7C11.4477 7 11 7.44772 11 8V13ZM13 15.9888C13 15.4365 12.5523 14.9888 12 14.9888C11.4477 14.9888 11 15.4365 11 15.9888V16C11 16.5523 11.4477 17 12 17C12.5523 17 13 16.5523 13 16V15.9888Z"
+                        fill="currentcolor" />
                 </svg>
             </i>
-            <span></span>
+            <span>حتما لنز دوربینت رو تمیز کنی که عکس واضح باشه</span>
         </div>
 
     </div>
